e95196fc996076a5ebc4819b25748b450ffc56ba
chiepomme <chie@loves-miko.cc>
2017-07-21 20:22:47 +0900

    initial commit

---
+++ Assets/Controller.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+using UTJ.FrameCapturer;
+
+public class Controller : MonoBehaviour
+{
+    [SerializeField]
+    MovieRecorder recorder;
+    [SerializeField]
+    RawImage image;
+
+    IEnumerator Start()
+    {
+        Screen.SetResolution(512, 512, false);
+
+        if (File.Exists("background.png"))
+        {
+            var imageWWW = new WWW("file://" + Application.dataPath + "/../background.png");
+            yield return imageWWW;
+            image.texture = imageWWW.texture;
+        }
+        else if (File.Exists("background.jpg"))
+        {
+            var imageWWW = new WWW("file://" + Application.dataPath + "/../background.jpg");
+            yield return imageWWW;
+            image.texture = imageWWW.texture;
+        }
+
+        var audiosource = gameObject.AddComponent<AudioSource>();
+        var audioWWW = new WWW("file://" + Application.dataPath + "/../audio.wav");
+        yield return audioWWW;
+        audiosource.clip = audioWWW.GetAudioClip(false);
+        audiosource.Play();
+
+        recorder.BeginRecording();
+        yield return new WaitWhile(() => audiosource.isPlaying);
+        recorder.EndRecording();
+        Application.Quit();
+    }
+}
---
+++ Assets/Visualizer.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Visualizer : MonoBehaviour
+{
+    Slider[] sliders;
+    float[] spectrum = new float[1024];
+    float[] bands;
+
+    void Awake()
+    {
+        sliders = GetComponentsInChildren<Slider>();
+        foreach (var slider in sliders)
+        {
+            slider.value = 0;
+        }
+
+        bands = new float[sliders.Length];
+    }
+
+    // TODO: 整理する
+    void Update()
+    {
+        var baseHz = (float)AudioSettings.outputSampleRate / spectrum.Length / 2;
+
+        AudioListener.GetSpectrumData(spectrum, 0, FFTWindow.BlackmanHarris);
+
+        var specI = 0;
+        var interval = (Mathf.Log((spectrum.Length - 1) * baseHz, 2) - Mathf.Log(1 * baseHz, 2)) / bands.Length / 4f; // 4?
+
+        for (var i = 0; i < bands.Length; i++)
+        {
+            var v = 0f;
+            var startSpecI = specI;
+            while (specI < spectrum.Length)
+            {
+                if (Mathf.Log(specI * baseHz, 2) - Mathf.Log(startSpecI * baseHz, 2) >= interval) break;
+                v += spectrum[specI];
+                specI++;
+            }
+
+            v /= specI - startSpecI;
+            bands[i] = v;
+        }
+
+        for (var i = 0; i < sliders.Length; i++)
+        {
+            var slider = sliders[i];
+            slider.value = bands[i] * 4f;
+        }
+    }
+}
