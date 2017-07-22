b584c95b9c250a8f3fafc705a5b1c778a8fa54f1
chiepomme <chie@loves-miko.cc>
2017-07-22 18:51:07 +0900

    OSX 向けにパスを修正

--- Assets/Controller.cs
+++ Assets/Controller.cs
@@ -18,6 +18,27 @@ public class Controller : MonoBehaviour
 
     bool useWebMInsteadOfMp4;
 
+    string GetFilePath(string relativePathFromRoot)
+    {
+        if (relativePathFromRoot.StartsWith("./"))
+        {
+            relativePathFromRoot = relativePathFromRoot.Substring(2);
+        }
+
+        string path;
+        switch (Application.platform)
+        {
+            case RuntimePlatform.OSXPlayer:
+                path = Application.dataPath + "/../../" + relativePathFromRoot;
+                break;
+            default:
+                path = Application.dataPath + "/../" + relativePathFromRoot;
+                break;
+        }
+
+        return new FileInfo(path).FullName.Replace("\\", "/");
+    }
+
     IEnumerator Start()
     {
         if (Application.platform == RuntimePlatform.OSXEditor || Application.platform == RuntimePlatform.OSXPlayer)
@@ -27,21 +48,21 @@ public class Controller : MonoBehaviour
 
         Screen.SetResolution(512, 512, false);
 
-        if (File.Exists("background.png"))
+        if (File.Exists(GetFilePath("background.png")))
         {
-            var imageWWW = new WWW("file://" + Application.dataPath + "/../background.png");
+            var imageWWW = new WWW("file://" + GetFilePath("background.png"));
             yield return imageWWW;
             image.texture = imageWWW.texture;
         }
-        else if (File.Exists("background.jpg"))
+        else if (File.Exists(GetFilePath("background.jpg")))
         {
-            var imageWWW = new WWW("file://" + Application.dataPath + "/../background.jpg");
+            var imageWWW = new WWW("file://" + GetFilePath("background.jpg"));
             yield return imageWWW;
             image.texture = imageWWW.texture;
         }
 
         var audiosource = gameObject.AddComponent<AudioSource>();
-        var audioWWW = new WWW("file://" + Application.dataPath + "/../audio.wav");
+        var audioWWW = new WWW("file://" + GetFilePath("audio.wav"));
         yield return audioWWW;
         audiosource.clip = audioWWW.GetAudioClip(false);
         audiosource.Play();
@@ -59,10 +80,10 @@ public class Controller : MonoBehaviour
 
         if (useWebMInsteadOfMp4)
         {
-            var webmPath = "\"" + LastPath + ".webm\"";
-            var mp4Path = "\"" + LastPath + ".mp4\"";
+            var webmPath = "\"" + GetFilePath(LastPath + ".webm") + "\"";
+            var mp4Path = "\"" + GetFilePath(LastPath + ".mp4") + "\"";
             var exeExtension = Application.platform == RuntimePlatform.WindowsEditor || Application.platform == RuntimePlatform.WindowsPlayer ? ".exe" : "";
-            var process = Process.Start(Application.dataPath + "/../ffmpeg/ffmpeg" + exeExtension, "-i " + webmPath + " " + mp4Path);
+            var process = Process.Start(GetFilePath("ffmpeg/ffmpeg" + exeExtension), "-i " + webmPath + " " + mp4Path);
             process.WaitForExit();
         }
 
